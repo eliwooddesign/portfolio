e170b190fc2f559098b6fd0d348c2805f44568e9
Eli Wood <mreliwood1@gmail.com>
2022-06-13 22:23:20 -0700

    adding found ark

--- src/components/pages/Work/index.js
+++ src/components/pages/Work/index.js
@@ -4,10 +4,10 @@ import './style.css';
 function Work({ currentPage, setCurrentPage }) {
 
     // set state
+    const [foundArkModifier, setFoundArkModifier] = useState(0);
     const [recipeModifier, setRecipeModifier] = useState(0);
     const [weatherModifier, setWeatherModifier] = useState(0);
     const [blogModifier, setBlogeModifier] = useState(0);
-    const [porefectionModifier, setPorefectionModifier] = useState(0);
     const [socialModifier, setSocialModifier] = useState(0);
     const [commerceModifier, setCommerceModifier] = useState(0);
 
@@ -18,16 +18,16 @@ function Work({ currentPage, setCurrentPage }) {
         const windowHeight = window.innerHeight;
 
         // get elements from the page
+        const foundArk = document.getElementById('found-ark').getBoundingClientRect().top;
         const recipe = document.getElementById('recipe').getBoundingClientRect().top;
         const weather = document.getElementById('weather').getBoundingClientRect().top;
         const blog = document.getElementById('blog').getBoundingClientRect().top;
-        const porefection = document.getElementById('porefection').getBoundingClientRect().top;
         const social = document.getElementById('social').getBoundingClientRect().top;
         const commerce = document.getElementById('commerce').getBoundingClientRect().top;
 
         // arrays of all elements and state setters
-        const allElements = [recipe, weather, blog, porefection, social, commerce];
-        const allStates = [setRecipeModifier, setWeatherModifier, setBlogeModifier, setPorefectionModifier, setSocialModifier, setCommerceModifier];
+        const allElements = [recipe, weather, blog, foundArk, social, commerce];
+        const allStates = [setRecipeModifier, setWeatherModifier, setBlogeModifier, setFoundArkModifier, setSocialModifier, setCommerceModifier];
 
         // calculate modifier and set to state
         for (let i = 0; i < allElements.length; i++) {
@@ -65,22 +65,52 @@ function Work({ currentPage, setCurrentPage }) {
 
             </div>
 
-            {/* recipe generator */}
-            <div id="recipe" className="project-preview-left">
+            {/* found ark */}
+            <div id="found-ark" className="project-preview-left">
 
-                <div className="preview-thumbnail">
+                <div className="preview-thumbnail-left">
 
-                    <a href="https://mreliwood.github.io/demo-recipe-generator/" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${recipeModifier}px)` }}>
-                        <img src="assets/work/recipe/desktop-mockup.png" alt="recipe generator desktop preview" className="project-thumbnail-desktop"></img>
+                    <a href="https://found-ark.herokuapp.com" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${foundArkModifier}px)` }}>
+                        <img src="assets/work/found-ark/desktop-mockup.png" alt="found ark desktop preview" className="project-thumbnail-desktop"></img>
+                        {foundArkModifier > 150 && <img src="assets/work/found-ark/desktop-animation.gif" alt="found ark desktop animation" className="thumbnail-animation-desktop"></img>}
                     </a>
 
-                    <a href="https://mreliwood.github.io/demo-recipe-generator/" target="_blank" rel="noreferrer" className="project-thumbnail-phone-link" style={{ transform: `translateY(-${Math.floor(recipeModifier * 0.4)}px)` }}>
-                        <img src="assets/work/recipe/mobile-mockup.png" alt="recipe generator phone preview" className="project-thumbnail-phone"></img>
+                    <a href="https://found-ark.herokuapp.com" target="_blank" rel="noreferrer" className="project-thumbnail-phone-link" style={{ transform: `translateY(${Math.floor(foundArkModifier * 0.5)}px)` }}>
+                        <img src="assets/work/found-ark/mobile-mockup.png" alt="found ark phone preview" className="project-thumbnail-phone"></img>
                     </a>
 
                 </div>
 
                 <div className="preview-details-right">
+
+                    <h2 className="preview-title">Found Ark</h2>
+
+                    <div className="preview-links">
+                        <a href="https://found-ark.herokuapp.com" target="_blank" rel="noreferrer">Deployed Application</a>
+                        <p className="preview-link-divider">|</p>
+                        <a href="https://github.com/chuanw101/found-ark" target="_blank" rel="noreferrer">GitHub Repository</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+            {/* recipe generator */}
+            <div id="recipe" className="project-preview-right">
+
+                <div className="preview-thumbnail-right">
+
+                    <a href="https://mreliwood.github.io/demo-recipe-generator/" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${recipeModifier}px)` }}>
+                        <img src="assets/work/recipe/desktop-mockup.png" alt="recipe generator desktop preview" className="project-thumbnail-desktop"></img>
+                    </a>
+
+                    <a href="https://mreliwood.github.io/demo-recipe-generator/" target="_blank" rel="noreferrer" className="project-thumbnail-phone-link" style={{ transform: `translateY(${Math.floor(recipeModifier * 0.5 - 125)}px)` }}>
+                        <img src="assets/work/recipe/mobile-mockup.png" alt="recipe generator phone preview" className="project-thumbnail-phone"></img>
+                    </a>
+
+                </div>
+
+                <div className="preview-details-left">
 
                     <h2 className="preview-title">Recipe Generator</h2>
 
@@ -95,9 +125,9 @@ function Work({ currentPage, setCurrentPage }) {
             </div>
 
             {/* weather dashboard */}
-            <div id="weather" className="project-preview-right">
+            <div id="weather" className="project-preview-left">
 
-                <div className="preview-thumbnail">
+                <div className="preview-thumbnail-left">
 
                     <a href="https://demo-weather-dashboard.herokuapp.com" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${weatherModifier}px)` }}>
                         <img src="assets/work/weather/desktop-mockup.png" alt="weather dashboard preview" className="project-thumbnail-desktop"></img>
@@ -105,7 +135,7 @@ function Work({ currentPage, setCurrentPage }) {
 
                 </div>
 
-                <div className="preview-details-left">
+                <div className="preview-details-right">
 
                     <h2 className="preview-title">Weather Dashboard</h2>
 
@@ -120,9 +150,9 @@ function Work({ currentPage, setCurrentPage }) {
             </div>
 
             {/* blog template */}
-            <div id="blog" className="project-preview-left">
+            <div id="blog" className="project-preview-right">
 
-                <div className="preview-thumbnail">
+                <div className="preview-thumbnail-right">
 
                     <a href="http://demo-blog-template.herokuapp.com/" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${blogModifier}px)` }}>
                         <img src="assets/work/blog/desktop-mockup.png" alt="blog template preview" className="project-thumbnail-desktop"></img>
@@ -130,7 +160,7 @@ function Work({ currentPage, setCurrentPage }) {
 
                 </div>
 
-                <div className="preview-details-right">
+                <div className="preview-details-left">
 
                     <h2 className="preview-title">Blog Template</h2>
 
@@ -144,35 +174,10 @@ function Work({ currentPage, setCurrentPage }) {
 
             </div>
 
-            {/* porefection */}
-            <div id="porefection" className="project-preview-right">
-
-                <div className="preview-thumbnail">
-
-                    <a href="https://porefect.herokuapp.com" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${porefectionModifier}px)` }}>
-                        <img src="assets/work/porefection/desktop-mockup.png" alt="porefection preview" className="project-thumbnail-desktop"></img>
-                    </a>
-
-                </div>
-
-                <div className="preview-details-left">
-
-                    <h2 className="preview-title">Porefect Skincare</h2>
-
-                    <div className="preview-links">
-                        <a href="https://porefect.herokuapp.com" target="_blank" rel="noreferrer">Deployed Application</a>
-                        <p className="preview-link-divider">|</p>
-                        <a href="https://github.com/jittel/combo-skincare" target="_blank" rel="noreferrer">GitHub Repository</a>
-                    </div>
-
-                </div>
-
-            </div>
-
             {/* social network api */}
-            <div id="social" className="project-preview-left">
+            <div id="social" className="project-preview-right">
 
-                <div className="preview-thumbnail">
+                <div className="preview-thumbnail-left">
 
                     <a href="https://github.com/MrEliWood/social-network-api" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${socialModifier}px)` }}>
                         <img src="assets/work/e-commerce/desktop-mockup.png" alt="social network api preview" className="project-thumbnail-desktop"></img>
@@ -180,7 +185,7 @@ function Work({ currentPage, setCurrentPage }) {
 
                 </div>
 
-                <div className="preview-details-right">
+                <div className="preview-details-left">
 
                     <h2 className="preview-title">Social Network API</h2>
 
@@ -193,9 +198,9 @@ function Work({ currentPage, setCurrentPage }) {
             </div>
 
             {/* e-commerce back end */}
-            <div id="commerce" className="project-preview-right">
+            <div id="commerce" className="project-preview-left">
 
-                <div className="preview-thumbnail">
+                <div className="preview-thumbnail-right">
 
                     <a href="https://github.com/MrEliWood/e-commerce-back-end" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${commerceModifier}px)` }}>
                         <img src="assets/work/e-commerce/desktop-mockup.png" alt="e-commerce back end preview" className="project-thumbnail-desktop"></img>
@@ -203,7 +208,7 @@ function Work({ currentPage, setCurrentPage }) {
 
                 </div>
 
-                <div className="preview-details-left">
+                <div className="preview-details-right">
 
                     <h2 className="preview-title">E-Commerce Back End</h2>
 
