7fc68e7088485e7507514f656c2b4e0cf7d15048
Eli Wood <mreliwood1@gmail.com>
2022-06-16 18:52:10 -0700

    reset due to file size issue

--- src/components/pages/Work/index.js
+++ src/components/pages/Work/index.js
@@ -11,6 +11,8 @@ function Work() {
     const [blogPos, setBlogePos] = useState();
     const [socialPos, setSocialPos] = useState();
     const [commercePos, setCommercePos] = useState();
+    const [mavenPos, setMavenPos] = useState();
+    const [propertyPos, setPropertyPos] = useState();
 
     // project modifiers
     const [foundArkMod, setFoundArkMod] = useState(0);
@@ -19,6 +21,8 @@ function Work() {
     const [blogMod, setBlogeMod] = useState(0);
     const [socialMod, setSocialMod] = useState(0);
     const [commerceMod, setCommerceMod] = useState(0);
+    const [mavenMod, setMavenMod] = useState(0);
+    const [propertyMod, setPropertyMod] = useState(0);
 
     // parallax effect
     const handleScroll = () => {
@@ -33,6 +37,8 @@ function Work() {
         const blog = document.getElementById('blog').getBoundingClientRect().top;
         const social = document.getElementById('social').getBoundingClientRect().top;
         const commerce = document.getElementById('commerce').getBoundingClientRect().top;
+        const maven = document.getElementById('maven').getBoundingClientRect().top;
+        const property = document.getElementById('property').getBoundingClientRect().top;
 
         setFoundArkPos(foundArk);
         setRecipePos(recipe);
@@ -40,10 +46,12 @@ function Work() {
         setBlogePos(blog);
         setSocialPos(social);
         setCommercePos(commerce);
+        setMavenPos(maven);
+        setPropertyPos(property);
 
         // arrays of all elements and state setters
-        const allPositions = [foundArk, recipe, weather, blog, social, commerce];
-        const setAllMods = [setFoundArkMod, setRecipeMod, setWeatherMod, setBlogeMod, setSocialMod, setCommerceMod];
+        const allPositions = [foundArk, recipe, weather, blog, social, commerce, maven, property];
+        const setAllMods = [setFoundArkMod, setRecipeMod, setWeatherMod, setBlogeMod, setSocialMod, setCommerceMod, setMavenMod, setPropertyMod];
 
         // set parallax end point
         let offset;
@@ -137,7 +145,7 @@ function Work() {
                         </div>
 
                         <p className={(foundArkPos / window.innerHeight) < 0.21 ? "project-description-right show-description" : "project-description-right hide-description"}>
-                            A web application that makes it easier for players to find and create groups for the game, Lost Ark, without having to rely on giant anonymous servers where they may never get picked up.
+                            A web application that makes it easier for players to find and create groups for the game Lost Ark, without having to rely on giant anonymous servers where they may never get picked up.
                         </p>
 
                     </div>
@@ -252,6 +260,82 @@ function Work() {
                 <div className="sticky-spacer-medium"></div>
 
             </div>
+
+            {/* maven group */}
+            <div className="sticky-container">
+
+                <div id="maven" className="project-preview-left">
+
+
+                    <div className="preview-thumbnail-left">
+
+                        <a href="https://www.mavengroupnw.com" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${Math.floor(mavenMod / 5)}px)` }}>
+                            <img src="./assets/work/maven/desktop-mockup.png" alt="maven group desktop preview" className="project-thumbnail-desktop"></img>
+                        </a>
+
+                        <a href="https://www.mavengroupnw.com" target="_blank" rel="noreferrer" className="project-thumbnail-phone-link" style={{ transform: `translateY(${Math.floor(mavenMod / 10)}px)` }}>
+                            <img src="./assets/work/maven/mobile-mockup.png" alt="maven group phone preview" className="project-thumbnail-phone"></img>
+                        </a>
+
+                    </div>
+
+                    <div className="preview-details-right">
+
+                        <h2 className="preview-title">Real Estate Team Websites</h2>
+
+                        <div className="preview-links">
+                            <a href="https://www.mavengroupnw.com" target="_blank" rel="noreferrer">Maven Group Live Website</a>
+                        </div>
+
+                        <p className={(mavenPos / window.innerHeight) < 0.21 ? "project-description-right show-description" : "project-description-right hide-description"}>
+                            Promotional websites for real estate teams, serving as the first point of contact for potential clients and showcase for the services and success of the team.
+                        </p>
+
+                    </div>
+
+                </div>
+
+                <div className="sticky-spacer-medium"></div>
+
+            </div>
+
+            {/* property website */}
+            <div className="sticky-container">
+
+                <div id="property" className="project-preview-right">
+
+                    <div className="preview-thumbnail-right">
+
+                        <a href="https://shadowhawktownhome.com" target="_blank" rel="noreferrer" className="project-thumbnail-desktop-link" style={{ transform: `translateY(-${propertyMod / 5}px)` }}>
+                            <img src="./assets/work/property/desktop-mockup.png" alt="property desktop preview" className="project-thumbnail-desktop"></img>
+                        </a>
+
+                        <a href="https://shadowhawktownhome.com" target="_blank" rel="noreferrer" className="project-thumbnail-phone-link" style={{ transform: `translateY(${propertyMod / 10}px)` }}>
+                            <img src="./assets/work/property/mobile-mockup.png" alt="property phone preview" className="project-thumbnail-phone"></img>
+                        </a>
+
+                    </div>
+
+                    <div className="preview-details-left">
+
+                        <h2 className="preview-title">Property Websites</h2>
+
+                        <div className="preview-links">
+                            <a href="https://shadowhawktownhome.com" target="_blank" rel="noreferrer">Shadowhawk Townhome Live Website</a>
+                        </div>
+
+                        <p className={(propertyPos / window.innerHeight) < 0.21 ? "project-description-left show-description" : "project-description-left hide-description"}>
+                            One of many websites shocasing a property for sale or for rent, featuring all important property details, engaging potential customers and funneling high value traffic.
+                        </p>
+
+                    </div>
+
+                </div>
+
+                <div className="sticky-spacer-medium"></div>
+
+            </div>
+
 
             {/* weather dashboard */}
             <div className="sticky-container">
