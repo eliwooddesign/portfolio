e663bf12e4735e286b7e2798b1da28e4c875a27e
Eli Wood <mreliwood1@gmail.com>
2022-11-07 17:11:28 -0800

    fixing styling bug with hero image and animation / reordering work samples to prioritize SWE

--- src/components/pages/Work/index.js
+++ src/components/pages/Work/index.js
@@ -22,16 +22,16 @@ function Work() {
 			</div>
 
 			{/* work */}
-			<Hero title='Maven Group' description='Experts in PNW real estate. Promotional website serving as the first touchpoint for potential clients and a modern showcase for the services and success of the team.' link='https://www.mavengroupnw.com' />
+			<Hero react title='Found Ark' description='A web application that makes it easier for players to find and create groups for the game Lost Ark, without having to rely on giant anonymous servers where they may never get picked up.' link='https://found-ark.herokuapp.com' repo='https://github.com/mreliwood/found-ark' />
+			<Standard flip react title='Porefection' description='This web application helps users manage their skincare routine by allowing them to create a detailed list of their skincare products. When the user adds a product to their list, detailed product information is fetched from Sephora API and the list is automatically sorted in the correct order for using the products.' link='https://mreliwood.github.io/porefection' repo='https://github.com/MrEliWood/porefection' />
+			<Standard title='Maven Group' description='Experts in PNW real estate. Promotional website serving as the first touchpoint for potential clients and a modern showcase for the services and success of the team.' link='https://www.mavengroupnw.com' />
 			<Standard flip title='Property Website' description='One of many websites showcasing a property for sale or for rent, featuring all important property details, engaging potential customers and funneling high value traffic.' link='https://shadowhawktownhome.com' />
-			<Standard react title='Found Ark' description='A web application that makes it easier for players to find and create groups for the game Lost Ark, without having to rely on giant anonymous servers where they may never get picked up.' link='https://found-ark.herokuapp.com' repo='https://github.com/mreliwood/found-ark' />
-			<Standard flip title='Recipe Generator' description='Users can find inspiration for their next meal by randomly generating recipes with sorting logic to filter their results by course and/or dietary restrictions and an engaging, intuitive user experience.' link='https://mreliwood.github.io/demo-recipe-generator/' repo='https://github.com/MrEliWood/demo-recipe-generator' />
-			<Standard react title='Porefection' description='This web application helps users manage their skincare routine by allowing them to create a detailed list of their skincare products. When the user adds a product to their list, detailed product information is fetched from Sephora API and the list is automatically sorted in the correct order for using the products.' link='https://mreliwood.github.io/porefection' repo='https://github.com/MrEliWood/porefection' />
-			<Standard flip single title='Blog Template' description='Free template for developers building a blog based on a MySQL database with complex relationships, including multiple users with hashed passwords powered by bcrypt.' link='http://demo-blog-template.herokuapp.com/' repo='https://github.com/MrEliWood/blog-template' />
-			<div className='sticky-container'>
+			<div className='background-wrapper'>
 				<Background fadeIn title='Social Network API' description='This Social Network API uses MongoDB to build the foundation for a NoSQL social network. Out of the box, users are able to share thoughts, react to thoughts, and connect with friends.' repo='https://github.com/mreliwood/social-network-api' />
 				<Background fadeIn title='E-Commerce Backend' description='This strictly backend e-commerce application allows the user to easily manage their inventory with a MySQL database or lays a solid foundation for a developer planning to build a full stack e-commerce platform.' repo='https://github.com/MrEliWood/e-commerce-back-end' />
 			</div>
+			<Standard flip title='Recipe Generator' description='Users can find inspiration for their next meal by randomly generating recipes with sorting logic to filter their results by course and/or dietary restrictions and an engaging, intuitive user experience.' link='https://mreliwood.github.io/demo-recipe-generator/' repo='https://github.com/MrEliWood/demo-recipe-generator' />
+			<Standard single title='Blog Template' description='Free template for developers building a blog based on a MySQL database with complex relationships, including multiple users with hashed passwords powered by bcrypt.' link='http://demo-blog-template.herokuapp.com/' repo='https://github.com/MrEliWood/blog-template' />
 		</div>
 	);
 }
