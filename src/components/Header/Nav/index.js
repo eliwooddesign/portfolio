8b79b89350f99660017e8b38b9b02afb7f70961a
Eli Wood <mreliwood1@gmail.com>
2022-11-19 19:28:50 -0800

    updated background component with reverse scrolling

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -3,67 +3,63 @@ import { Link, useLocation } from 'react-router-dom';
 import './style.css';
 
 function Nav({ handleMobileNavClick, navStatus, setNavStatus }) {
+	const location = useLocation();
 
-    const location = useLocation();
+	const navDetail = '/';
 
-    const navDetail = '/';
+	const handleMobileNav = () => {
+		if (window.innerWidth > 768) {
+			setNavStatus('');
+		} else {
+			setNavStatus('hidden');
+		}
+	};
 
-    const handleMobileNav = () => {
+	useEffect(() => {
+		window.addEventListener('resize', handleMobileNav);
+		handleMobileNav();
+		// eslint-disable-next-line
+	}, []);
 
-        if (window.innerWidth > 768) {
-            setNavStatus('');
-        } else {
-            setNavStatus('hidden');
-        };
+	return (
+		<>
+			<div onClick={handleMobileNavClick} className='nav-item mobile-nav-button'>
+				<p className={navStatus === 'nav-open' ? 'animate-nav-detail-left' : ''}>{navDetail}</p>
+				<p className={navStatus === 'nav-open' ? 'animate-nav-detail-right' : ''}>{navDetail}</p>
+			</div>
 
-    };
+			<ul onClick={handleMobileNavClick} className={'nav ' + navStatus}>
+				<li className='nav-item nav-detail'>
+					<p>{navDetail}</p>
+					<p>{navDetail}</p>
+				</li>
 
-    useEffect(() => {
-        window.addEventListener("resize", handleMobileNav);
-        handleMobileNav();
-        // eslint-disable-next-line
-    }, []);
+				<li className='nav-item'>
+					<Link to='about' className={'nav-link ' + (location.pathname === '/about' && 'active')}>
+						About
+					</Link>
+				</li>
 
-    return (
+				<li className='nav-item'>
+					<Link to='work' className={location.pathname === '/work' || location.pathname === '/' || location.pathname === '' ? 'nav-link active' : 'nav-link'}>
+						Work
+					</Link>
+				</li>
 
-        <>
+				<li className='nav-item'>
+					<Link to='contact' className={'nav-link ' + (location.pathname === '/contact' && 'active')}>
+						Contact
+					</Link>
+				</li>
 
-            <div onClick={handleMobileNavClick} className="nav-item mobile-nav-button">
-                <p className={navStatus === 'nav-open' && 'animate-nav-detail-left'}>{navDetail}</p>
-                <p className={navStatus === 'nav-open' && 'animate-nav-detail-right'}>{navDetail}</p>
-            </div>
-
-            <ul onClick={handleMobileNavClick} className={"nav " + navStatus}>
-
-                <li className="nav-item nav-detail">
-                    <p>{navDetail}</p>
-                    <p>{navDetail}</p>
-                </li>
-
-                <li className="nav-item">
-                    <Link to="about" className={"nav-link " + (location.pathname === '/about' && 'active')}>About</Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link to="work" className={location.pathname === '/work' || location.pathname === '/' || location.pathname === '' ? 'nav-link active' : 'nav-link'}>Work</Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link to="contact" className={"nav-link " + (location.pathname === '/contact' && 'active')}>Contact</Link>
-                </li>
-
-                <li className="nav-item">
-                    <a href="./assets/resume/eli-wood-resume.pdf" target="_blank" rel="noreferrer" className="nav-link">
-                        Resume
-                    </a>
-                </li>
-
-            </ul>
-
-        </>
-
-    );
-
-};
+				<li className='nav-item'>
+					<a href='./assets/resume/eli-wood-resume.pdf' target='_blank' rel='noreferrer' className='nav-link'>
+						Resume
+					</a>
+				</li>
+			</ul>
+		</>
+	);
+}
 
 export default Nav;
--- src/components/pages/Work/types/Background/index.js
+++ src/components/pages/Work/types/Background/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 function Background({ title, description, link, repo, react, flip, fadeIn, fadeOut }) {
 	// create id based on title
 	const id = title.replace(/ /g, '-').toLowerCase();
+
+	// set default background position
+	const [backgroundPosition, setBackgroundPosition] = useState(-window.innerHeight);
 
 	// set background based on props and screen size
 	let styles;
@@ -11,63 +14,89 @@ function Background({ title, description, link, repo, react, flip, fadeIn, fadeO
 	if (fadeIn) {
 		window.innerWidth > 1024
 			? (styles = {
-					backgroundImage: `url('./assets/work/${id}/background.png')`
+					backgroundImage: `url('./assets/work/${id}/background.png')`,
+					top: `${backgroundPosition}px`
 			  })
 			: (styles = {
-					backgroundImage: `linear-gradient(var(--background90), var(--background50)), url('./assets/work/${id}/background.png')`,
-					height: '50vh'
+					backgroundImage: `linear-gradient(var(--background), var(--background75)), url('./assets/work/${id}/background.png')`,
+					position: 'static'
 			  });
 	} else if (fadeOut) {
 		window.innerWidth > 1024
 			? (styles = {
-					backgroundImage: `url('./assets/work/${id}/background.png')`
+					backgroundImage: `url('./assets/work/${id}/background.png')`,
+					top: `${backgroundPosition}px`
 			  })
 			: (styles = {
-					backgroundImage: `linear-gradient(var(--background50), var(--background)), url('./assets/work/${id}/background.png')`,
-					height: '50vh'
+					backgroundImage: `linear-gradient(var(--background75), var(--background)), url('./assets/work/${id}/background.png')`,
+					position: 'static'
 			  });
 	} else {
 		window.innerWidth > 1024
 			? (styles = {
-					backgroundImage: `url('./assets/work/${id}/background.png')`
+					backgroundImage: `url('./assets/work/${id}/background.png')`,
+					top: `${backgroundPosition}px`
 			  })
 			: (styles = {
-					backgroundImage: `linear-gradient(var(--background50), var(--background50)), url('./assets/work/${id}/background.png')`,
-					height: '50vh'
+					backgroundImage: `linear-gradient(var(--background75), var(--background75)), url('./assets/work/${id}/background.png')`,
+					position: 'static'
 			  });
 	}
 
+	// scroll effect
+	const handleScroll = () => {
+		// get elements from the page
+		const position = document.getElementById(id).getBoundingClientRect().top;
+
+		if (position <= window.innerHeight) {
+			setBackgroundPosition(0 - position);
+		} else if (position > window.innerHeight) {
+			setBackgroundPosition(-window.innerHeight);
+		}
+	};
+
+	// listen for scroll event
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		handleScroll();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
-		<div id={id} className='project-preview-left background' style={styles}>
-			<div className='placeholder-container' style={{ width: '50%' }}></div>
+		<>
+			<div id={'background-' + id} className='background-scroll-image' style={window.innerWidth > 1024 ? styles : { display: 'none' }} />
 
-			<div className='preview-details-right'>
-				<h2 className='preview-title'>{title}</h2>
+			<div id={id} className='project-preview-background' style={window.innerWidth <= 1024 ? styles : { display: 'flex' }}>
+				<div className='placeholder-container' style={{ width: '50%' }}></div>
 
-				<p className='project-description-right'>{description}</p>
+				<div className='preview-details-background'>
+					<h2 className='preview-title'>{title}</h2>
 
-				<div className='preview-links'>
-					{link && (
-						<a href={link} target='_blank' rel='noreferrer' className={flip ? 'icon-link-left' : 'icon-link-right'}>
-							<img src='./assets/icons/web-icon.png' alt='deployed application link' className='icon'></img>
-							<p className={flip ? 'icon-description-left' : 'icon-description-right'}>Deployed Application</p>
-						</a>
-					)}
-					{repo && (
-						<a href={repo} target='_blank' rel='noreferrer' className={flip ? 'icon-link-left' : 'icon-link-right'}>
-							<img src='./assets/icons/GitHub-Mark-Light-120px-plus.png' alt='GitHub repository link' className='icon'></img>
-							<p className={flip ? 'icon-description-left' : 'icon-description-right'}>GitHub Repository</p>
-						</a>
-					)}
-					{react && (
-						<div className={flip ? 'icon-link-left' : 'icon-link-right'}>
-							<img src='./assets/icons/React-icon.png' alt='GitHub repository link' className='icon'></img>
-							<p className={flip ? 'icon-description-left' : 'icon-description-right'}>Made with React</p>
-						</div>
-					)}
+					<p className='project-description-right'>{description}</p>
+
+					<div className='preview-links'>
+						{link && (
+							<a href={link} target='_blank' rel='noreferrer' className={flip ? 'icon-link-left' : 'icon-link-right'}>
+								<img src='./assets/icons/web-icon.png' alt='deployed application link' className='icon'></img>
+								<p className={flip ? 'icon-description-left' : 'icon-description-right'}>Deployed Application</p>
+							</a>
+						)}
+						{repo && (
+							<a href={repo} target='_blank' rel='noreferrer' className={flip ? 'icon-link-left' : 'icon-link-right'}>
+								<img src='./assets/icons/GitHub-Mark-Light-120px-plus.png' alt='GitHub repository link' className='icon'></img>
+								<p className={flip ? 'icon-description-left' : 'icon-description-right'}>GitHub Repository</p>
+							</a>
+						)}
+						{react && (
+							<div className={flip ? 'icon-link-left' : 'icon-link-right'}>
+								<img src='./assets/icons/React-icon.png' alt='GitHub repository link' className='icon'></img>
+								<p className={flip ? 'icon-description-left' : 'icon-description-right'}>Made with React</p>
+							</div>
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
