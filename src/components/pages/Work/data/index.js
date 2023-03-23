7936d7d35fbfd4e83c5ee9087c2af000255a499f
Eli Wood <mreliwood1@gmail.com>
2023-03-23 13:10:36 -0700

    updating domain

---
+++ src/components/pages/Work/data/index.js
@@ -0,0 +1,17 @@
+const workData = {
+	propertyWebsite: {
+		title: 'Property Website',
+		description: 'Promotional websites showcasing a property for sale or for rent, featuring all important property details, engaging potential customers and funneling high value traffic. Designed and built from scratch and successfully deployed several dozen over the last four years.',
+		link: 'https://mreliwood.github.io/demo-property/',
+		repo: 'https://github.com/MrEliWood/demo-property',
+		react: true
+	},
+
+	mavenGroup: {
+		title: 'Maven Group',
+		description: 'Full stack promotional website serving as the first touchpoint for potential clients and a modern showcase for the services and success of the team.',
+		link: 'https://www.mavengroupnw.com'
+	}
+};
+
+export default workData;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <BrowserRouter basename="/portfolio">
-        <App />
-    </BrowserRouter>
+	<BrowserRouter basename='/portfolio'>
+		<App />
+	</BrowserRouter>
 );
