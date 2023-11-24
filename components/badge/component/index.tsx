8dd5a3e3fa5a09af2bc014e0802848e3bc1b2908
Eli Wood <mreliwood1@gmail.com>
2023-11-23 17:51:55 -0800

    modularized badge component

---
+++ components/badge/component/index.tsx
@@ -0,0 +1,19 @@
+import Image, { StaticImageData } from 'next/image';
+
+import styles from './style.module.css';
+
+type Props = {
+	image: StaticImageData;
+	label: string;
+};
+
+export default function Component(props: Props) {
+	const { image, label } = props;
+
+	return (
+		<div className={styles.badge_container}>
+			<Image src={image} alt={`${label} badge`} className={styles.badge} />
+			{label && <h5 className={styles.badge_label}>{label}</h5>}
+		</div>
+	);
+}
--- components/badge/index.tsx
+++ components/badge/index.tsx
@@ -1,36 +1,26 @@
-import Image, { StaticImageData } from 'next/image';
-
-import styles from './style.module.css';
+import Component from './component';
 
 import { badges } from '@/assets';
-
 const { adobe__cc, bootstrap, css, express, figma, handlebars, html, javascript, jquery, mongodb, mysql, next_js, node_js, react_js, sequelize, sql, typescript } = badges;
 
-const createBadge = (image: StaticImageData, label: string) => (
-	<div className={styles.badge_container}>
-		<Image src={image} alt={`${label} badge`} className={styles.badge} />
-		{label && <h5 className={styles.badge_label}>{label}</h5>}
-	</div>
-);
-
 const Badge = {
-	Adobe__CC: () => createBadge(adobe__cc, 'Adobe CC'),
-	Bootstrap: () => createBadge(bootstrap, 'Bootstrap'),
-	CSS: () => createBadge(css, 'CSS'),
-	Express: () => createBadge(express, 'Express'),
-	Figma: () => createBadge(figma, 'Figma'),
-	Handlebars: () => createBadge(handlebars, 'Handlebars'),
-	Html: () => createBadge(html, 'HTML'),
-	JavaScript: () => createBadge(javascript, 'JavaScript'),
-	jQuery: () => createBadge(jquery, 'jQuery'),
-	MongoDB: () => createBadge(mongodb, 'MongoDB'),
-	MySQL: () => createBadge(mysql, 'MySQL'),
-	Next_js: () => createBadge(next_js, 'Next.js'),
-	Node_js: () => createBadge(node_js, 'Node.js'),
-	React_js: () => createBadge(react_js, 'React.js'),
-	Sequelize: () => createBadge(sequelize, 'Sequelize'),
-	SQL: () => createBadge(sql, 'SQL'),
-	TypeScript: () => createBadge(typescript, 'TypeScript')
+	Adobe__CC: () => <Component image={adobe__cc} label='Adobe CC' />,
+	Bootstrap: () => <Component image={bootstrap} label='Bootstrap' />,
+	CSS: () => <Component image={css} label='CSS' />,
+	Express: () => <Component image={express} label='Express' />,
+	Figma: () => <Component image={figma} label='Figma' />,
+	Handlebars: () => <Component image={handlebars} label='Handlebars' />,
+	Html: () => <Component image={html} label='HTML' />,
+	JavaScript: () => <Component image={javascript} label='JavaScript' />,
+	jQuery: () => <Component image={jquery} label='jQuery' />,
+	MongoDB: () => <Component image={mongodb} label='MongoDB' />,
+	MySQL: () => <Component image={mysql} label='MySQL' />,
+	Next_js: () => <Component image={next_js} label='Next.js' />,
+	Node_js: () => <Component image={node_js} label='Node.js' />,
+	React_js: () => <Component image={react_js} label='React.js' />,
+	Sequelize: () => <Component image={sequelize} label='Sequelize' />,
+	SQL: () => <Component image={sql} label='SQL' />,
+	TypeScript: () => <Component image={typescript} label='TypeScript' />
 };
 
 export default Badge;
